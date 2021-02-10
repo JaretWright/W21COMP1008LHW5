3e0cd34dc641463252d61bfa208fb47279926a57
Jaret Wright <hkkA223>
2021-02-10 11:41:02 -0500

    Purchases instance variable and constructor complete

--- src/Purchases.java
+++ src/Purchases.java
@@ -1,2 +1,9 @@
+import java.util.ArrayList;
+
 public class Purchases {
+    private ArrayList<GroceryItem> purchases;
+
+    public Purchases() {
+        this.purchases = new ArrayList<>();
+    }
 }
